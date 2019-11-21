dea85f8186b6230f7ed4dd5052842158a4f10ef0
Austin Brown <ddlinkab@gmail.com>
2019-11-20 20:28:31 -0500

    Add essentials for start

---
+++ src/component/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './nav';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div>
+                Austin Brown
+                <Nav />
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/component/nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Nav extends React.Component {
+    render(){
+        return(
+            <div>
+                <div>
+                    Projects
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Info
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Nav;
